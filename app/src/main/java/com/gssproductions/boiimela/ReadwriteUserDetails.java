47e65bafc98c88609fe54ddd1e507f500e4c3221
Gourab <gourab3901@gmail.com>
2023-04-04 02:02:02 +0530

    reg and log

---
+++ app/src/main/java/com/gssproductions/boiimela/ReadwriteUserDetails.java
@@ -0,0 +1,13 @@
+package com.gssproductions.boiimela;
+
+public class ReadwriteUserDetails {
+
+    public String doB,gender,mobile;
+    public ReadwriteUserDetails(String textDob,String textGender,String textMobile){
+
+        this.doB=textDob;
+        this.gender=textGender;
+        this.mobile=textMobile;
+    }
+
+}
