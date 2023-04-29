4dd335373d57d9194ab612e39772bb5c5142c708
Gourab <gourab3901@gmail.com>
2023-04-30 01:28:59 +0530

    edit email

---
+++ app/src/main/java/com/gssproductions/boiimela/UpdateEmailActivity.java
@@ -0,0 +1,155 @@
+package com.gssproductions.boiimela;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.EmailAuthProvider;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class UpdateEmailActivity extends AppCompatActivity {
+    private FirebaseAuth authProfile;
+    private FirebaseUser firebaseUser;
+    private ProgressBar progressBar;
+    private TextView textViewAuthenticated;
+    private String userOldEmail,userNewEmail,userPassword;
+    private Button buttonUpdateEmail;
+    private EditText editTextNewEmail,editTextPwd;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_update_email);
+
+        progressBar=findViewById(R.id.progressBar_update_email);
+        editTextPwd=findViewById(R.id.editText_update_email_verify_password);
+        editTextNewEmail=findViewById(R.id.editText_update_email_new);
+        textViewAuthenticated=findViewById(R.id.textView_update_email_authenticated);
+        buttonUpdateEmail=findViewById(R.id.button_update_email);
+
+        buttonUpdateEmail.setEnabled(false);
+        editTextNewEmail.setEnabled(false);
+
+        authProfile=FirebaseAuth.getInstance();
+        firebaseUser=authProfile.getCurrentUser();
+
+        //set old email on textview
+        userOldEmail=firebaseUser.getEmail();
+        TextView textViewOldEmail=findViewById(R.id.textView_update_email_old);
+        textViewOldEmail.setText(userOldEmail);
+
+        if (firebaseUser.equals("")) {
+            Toast.makeText(UpdateEmailActivity.this, "user's data not available", Toast.LENGTH_SHORT).show();
+        }else {
+            reAuthenticate(firebaseUser);
+        }
+    }
+
+    //verify user before update
+    private void reAuthenticate(FirebaseUser firebaseUser) {
+        Button buttonVerifyUser=findViewById(R.id.button_authenticate_user);
+        buttonVerifyUser.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //obtain password for authentication
+                userPassword=editTextPwd.getText().toString();
+                if (TextUtils.isEmpty(userPassword)){
+                    Toast.makeText(UpdateEmailActivity.this, "password needed", Toast.LENGTH_SHORT).show();
+                    editTextPwd.setError("please enter your password");
+                    editTextPwd.requestFocus();
+                }else {
+                    progressBar.setVisibility(View.VISIBLE);
+
+                    AuthCredential credential= EmailAuthProvider.getCredential(userOldEmail,userPassword);
+                    firebaseUser.reauthenticate(credential).addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+                            if (task.isSuccessful()){
+                                progressBar.setVisibility(View.GONE);
+                                Toast.makeText(UpdateEmailActivity.this, "password has been verified"+"you can update your email", Toast.LENGTH_SHORT).show();
+                                //set textview to show user is authenticated
+                                textViewAuthenticated.setText("you can change your email now");
+
+                                //disable edittext for password ,button to verify user and enable edittext for new email and update email button
+                                editTextNewEmail.setEnabled(true);
+                                editTextPwd.setEnabled(false);
+                                buttonVerifyUser.setEnabled(false);
+                                buttonUpdateEmail.setEnabled(true);
+
+                                buttonUpdateEmail.setBackgroundTintList(ContextCompat.getColorStateList(UpdateEmailActivity.this,R.color.green));
+
+                                buttonUpdateEmail.setOnClickListener(new View.OnClickListener() {
+                                    @Override
+                                    public void onClick(View view) {
+                                        userNewEmail=editTextNewEmail.getText().toString();
+                                        if (TextUtils.isEmpty(userNewEmail)){
+                                            Toast.makeText(UpdateEmailActivity.this, "new email is required", Toast.LENGTH_SHORT).show();
+                                            editTextNewEmail.setError("please enter new email");
+                                            editTextNewEmail.requestFocus();
+                                        } else if (!Patterns.EMAIL_ADDRESS.matcher(userNewEmail).matches()) {
+                                            Toast.makeText(UpdateEmailActivity.this, "please enter valid email", Toast.LENGTH_SHORT).show();
+                                            editTextNewEmail.setError("please enter valid email");
+                                            editTextNewEmail.requestFocus();
+                                        } else if (userOldEmail.matches(userNewEmail)) {
+                                            Toast.makeText(UpdateEmailActivity.this, "new email cannot be same as old email", Toast.LENGTH_SHORT).show();
+                                            editTextNewEmail.setError("please enter new email");
+                                            editTextNewEmail.requestFocus();
+                                        }else {
+                                            progressBar.setVisibility(View.VISIBLE);
+                                            updateEmail(firebaseUser);
+                                        }
+                                    }
+                                });
+                            }else {
+                                try {
+                                    throw task.getException();
+                                }catch (Exception e){
+                                    Toast.makeText(UpdateEmailActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        }
+                    });
+                }
+            }
+        });
+    }
+
+    private void updateEmail(FirebaseUser firebaseUser) {
+        firebaseUser.updateEmail(userNewEmail).addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+                if (task.isComplete()){
+                    //verify email
+                    firebaseUser.sendEmailVerification();
+                    Toast.makeText(UpdateEmailActivity.this, "please verify", Toast.LENGTH_SHORT).show();
+                    Intent intent =new Intent(UpdateEmailActivity.this,ProfileFragment.class);
+                    startActivity(intent);
+                    finish();
+                }else {
+                    try {
+                        throw task.getException();
+                    }catch (Exception e){
+                        Toast.makeText(UpdateEmailActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                }
+                progressBar.setVisibility(View.GONE);
+            }
+        });
+    }
+}
