82c01a6b226ef12e0691a5ae2395966182455259
Saqib Javed <jvedsaqib6291@gmail.com>
2023-04-30 23:50:42 +0530

    changes on book view - Saqib

--- app/src/main/java/com/gssproductions/boiimela/BookFragment.java
+++ app/src/main/java/com/gssproductions/boiimela/BookFragment.java
@@ -13,6 +13,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -37,11 +39,20 @@ public class BookFragment extends Fragment {
     TextView book_title, book_price,
             cover_type, tv_bookCondition_data,
             tv_bookCategory_data, tv_bookOtherCategory_data,
-            tv_bookUserDesc_data;
+            tv_bookUserDesc_data, tv_sellerName_data,
+            tv_sellerAddress_data, tv_sellerEmail_data,
+            tv_sellerNumber_data, tv_sellerNumber_ask,
+            tv_bookTitle_data, tv_bookAuthor_data,
+            tv_bookPublisher_data;
+
+    EditText et_offer_price;
+    RelativeLayout offer_layout;
+
 
     String[] imgUrls;
 
-    Button user_chat_btn;
+    Button user_chat_btn, makeOffer_btn,
+            negotiate_btn;
 
     public BookFragment(BookData ob){
         this.ob = ob;
@@ -84,6 +95,10 @@ public class BookFragment extends Fragment {
 
         ViewPageAdapter adapter = new ViewPageAdapter(getContext(), imgUrls);
 
+        offer_layout = view.findViewById(R.id.offer_layout);
+        offer_layout.setEnabled(false);
+        offer_layout.setVisibility(View.INVISIBLE);
+
         imageViewPager = view.findViewById(R.id.imageViewPager);
         imageViewPager.setAdapter(adapter);
 
@@ -116,6 +131,52 @@ public class BookFragment extends Fragment {
             tv_bookUserDesc_data.setText(ob.getDescription());
         }
 
+        // ---------------- book details view ----------------
+
+        tv_bookTitle_data = view.findViewById(R.id.tv_bookTitle_data);
+        tv_bookTitle_data.setText(ob.getTitle());
+
+        tv_bookAuthor_data = view.findViewById(R.id.tv_bookAuthor_data);
+        tv_bookAuthor_data.setText(ob.getAuthorName());
+
+        tv_bookPublisher_data = view.findViewById(R.id.tv_bookPublisher_data);
+        tv_bookPublisher_data.setText(ob.getPublisherName());
+
+
+        // ---------------- seller details view ---------------
+
+        tv_sellerName_data = view.findViewById(R.id.tv_sellerName_data);
+        tv_sellerName_data.setText(ob.getSeller_name());
+
+        tv_sellerAddress_data = view.findViewById(R.id.tv_sellerAddress_data);
+        tv_sellerAddress_data.setText(ob.getAddress());
+
+//        tv_sellerEmail_data = view.findViewById(R.id.tv_sellerEmail_data);
+//        tv_sellerEmail_data.setText("ob.getEmail()");
+
+        tv_sellerNumber_data = view.findViewById(R.id.tv_sellerNumber_data);
+        String numbCat = "+91 XXXXXXX"+ob.getPhoneNumber().substring(ob.getPhoneNumber().length() - 3);
+        tv_sellerNumber_data.setText(numbCat);
+
+        tv_sellerNumber_ask = view.findViewById(R.id.tv_sellerNumber_ask);
+        if(FirebaseAuth.getInstance().getCurrentUser().getUid().equals(ob.getUid())){
+            tv_sellerNumber_ask.setVisibility(View.INVISIBLE);
+        }
+        tv_sellerNumber_ask.setOnClickListener(v -> {
+            context = getContext();
+            String message = "Hey Papi send me your number !";
+            if(FirebaseAuth.getInstance().getCurrentUser().isEmailVerified()){
+                AppCompatActivity activity = (AppCompatActivity) context;
+                activity.getSupportFragmentManager()
+                        .beginTransaction()
+                        .replace(R.id.fragment_layout, new UserChatFragment(ob, message))
+                        .commit();
+            }
+            else{
+                Toast.makeText(context, "Please verify your email first", Toast.LENGTH_SHORT).show();
+            }
+
+        });
 
 
         user_chat_btn = (Button) view.findViewById(R.id.user_chat_btn);
@@ -138,6 +199,36 @@ public class BookFragment extends Fragment {
                 Toast.makeText(context, "Please verify your email first", Toast.LENGTH_SHORT).show();
             }
 
+        });
+
+        makeOffer_btn = (Button) view.findViewById(R.id.makeOffer_btn);
+        if(ob.getUid().equals(FirebaseAuth.getInstance().getCurrentUser().getUid())){
+            makeOffer_btn.setEnabled(false);
+        }
+
+        makeOffer_btn.setOnClickListener(v -> {
+            offer_layout.setEnabled(true);
+            offer_layout.setVisibility(View.VISIBLE);
+            et_offer_price = view.findViewById(R.id.et_offer_price);
+            et_offer_price.setText(ob.getPrice());
+
+            negotiate_btn = (Button) view.findViewById(R.id.negotiate_btn);
+            negotiate_btn.setOnClickListener(vi -> {
+                context = getContext();
+
+                String message = "Hey Papi, can I get it @ Rs."+et_offer_price.getText().toString();
+
+                if(FirebaseAuth.getInstance().getCurrentUser().isEmailVerified()){
+                    AppCompatActivity activity = (AppCompatActivity) context;
+                    activity.getSupportFragmentManager()
+                            .beginTransaction()
+                            .replace(R.id.fragment_layout, new UserChatFragment(ob, message))
+                            .commit();
+                }
+                else{
+                    Toast.makeText(context, "Please verify your email first", Toast.LENGTH_SHORT).show();
+                }
+            });
         });
 
 
