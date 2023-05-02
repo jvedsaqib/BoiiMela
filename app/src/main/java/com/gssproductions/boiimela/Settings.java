472fe2c4eb20377a93263edb62888b215320d443
swarnadeep31 <swarnadeeproy69@gmail.com>
2023-05-02 12:03:57 +0530

    night_mode

--- app/src/main/java/com/gssproductions/boiimela/Settings.java
+++ app/src/main/java/com/gssproductions/boiimela/Settings.java
@@ -1,20 +1,31 @@
 package com.gssproductions.boiimela;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CompoundButton;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+
+import com.google.android.material.switchmaterial.SwitchMaterial;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class Settings extends AppCompatActivity {
 
+
+    private View parentView;
+    private SwitchMaterial themeSwitch;
+    private TextView themeTV, titleTV;
+
+    private UserSettings settings;
     RelativeLayout email_verify_block;
 
     ImageView back_to_frag_btn;
@@ -22,6 +33,13 @@ public class Settings extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+
+        settings = (UserSettings) getApplication();
+
+        initWidgets();
+        loadSharedPreferences();
+        initSwitchListener();
+
 
         //edit profile
         Button buttonUpdateProfile=findViewById(R.id.buttonEditProfile);
@@ -50,6 +68,62 @@ public class Settings extends AppCompatActivity {
             startActivity(new Intent(Settings.this, BaseActivity.class));
             finish();
         });
+
+    }
+
+    private void initSwitchListener() {
+    themeSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+        @Override
+        public void onCheckedChanged(CompoundButton compoundButton, boolean checked) {
+
+            if(checked)
+                settings.setCustomTheme(UserSettings.DARK_THEME);
+            else
+                settings.setCustomTheme(UserSettings.LIGHT_THEME);
+
+            SharedPreferences.Editor editor = getSharedPreferences(UserSettings.PREFERENCESS,MODE_PRIVATE).edit();
+            editor.putString(UserSettings.CUSTOM_THEME,settings.getCustomTheme());
+            editor.apply();
+            updateView();
+        }
+
+        private void updateView() {
+            final int black = ContextCompat.getColor(this, R.color.black);
+            final int white = ContextCompat.getColor(this, R.color.white);
+
+            if(settings.getCustomTheme().equals(UserSettings.DARK_THEME))
+            {
+                titleTV.setTextColor(white);
+                themeTV.setTextColor(white);
+                themeTV.setText("Dark");
+                parentView.setBackgroundColor(black);
+                themeSwitch.setChecked(true);
+            }
+            else
+            {
+                titleTV.setTextColor(black);
+                themeTV.setTextColor(black);
+                themeTV.setText("Light");
+                parentView.setBackgroundColor(white);
+                themeSwitch.setChecked(false);
+            }
+
+        }
+    });
+
+    }
+
+    private void loadSharedPreferences() {
+        SharedPreferences sharedPreferences = getSharedPreferences(UserSettings.PREFERENCESS,MODE_PRIVATE);
+        String theme = sharedPreferences.getString(UserSettings.CUSTOM_THEME,UserSettings.LIGHT_THEME);
+        settings.setCustomTheme(theme);
+    }
+
+    private void initWidgets() {
+        themeTV = findViewById(R.id.themeTV);
+        titleTV = findViewById(R.id.titleTV);
+        themeSwitch = findViewById(R.id.themeSwitchTV);
+        parentView = findViewById(R.id.parentView);
 
     }
 }
---
+++ app/src/main/java/com/gssproductions/boiimela/UserSettings.java
@@ -0,0 +1,21 @@
+package com.gssproductions.boiimela;
+
+import android.app.Application;
+
+public class UserSettings extends Application {
+
+    public static final String PREFERENCESS = "prefencess";
+    public static final String CUSTOM_THEME = "customTheme";
+    public static final String LIGHT_THEME = "lightTheme";
+    public static final String DARK_THEME = "darkTheme";
+
+    private String customTheme;
+
+    public String getCustomTheme() {
+        return customTheme;
+    }
+
+    public void setCustomTheme(String customTheme) {
+        this.customTheme = customTheme;
+    }
+}
